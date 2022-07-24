58a3a309b7b2c41caa467ba2b2aec1c6f3b52d12
SaRu-N <saruniroula73@gmail.com>
2022-07-24 05:57:31 +0545

    Small Changes

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,15 +1,22 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace MvcMovie.Models
 {
     public class Movie
     {
         public int Id { get; set; }
+
+        [Display(Name = "Movie Title")]
         public string? Title { get; set; }
 
+      
+        [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string? Genre { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }
